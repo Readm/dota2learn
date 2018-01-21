ee3c69563aafa9c7d0a54a851239d336cbc47e61
Readm <lijinfeng@iie.ac.cn>
2018-01-21 22:12:18 +0800

    first version

--- monitor.py
+++ monitor.py
@@ -7,11 +7,10 @@ vh_crawler = crawler.Crawler(1)
 crawlers = [n_crawler, h_crawer, vh_crawler]
 interval = [config.getConfig("crawler", "n"), config.getConfig("crawler", "h"), config.getConfig("crawler", "vh")]
 
-last_run_time = [time.time() for i in range(3)]
+last_run_time = [time.time()-interval[i]*60 for i in range(3)]
 while True:
     time_now = time.time()
     for i in range(3):
-        print time_now - last_run_time[i], interval[i]
         if time_now - last_run_time[i] > int(interval[i]) * 60:
             if os.fork()==0:
                 crawler[i].run()
